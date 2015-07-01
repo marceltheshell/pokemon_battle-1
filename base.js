405d82fb475b7e8da18654b78a11a08c698a7fa2
nathanallen <thenathanator@gmail.com>
2015-07-01 15:50:45 -0700

    pick two players

--- base.js
+++ base.js
@@ -3,8 +3,13 @@ var app = angular.module('pokemonApp', []);
 app.controller('PokemonCtrl', function($scope) {
 
   $scope.pokemon = res.results.collection1; // see mock_data.js
-  $scope.iChooseYou = function(src){
-    $scope.image_src = src;
+
+  $scope.iChooseYou = function(pokemon){
+    if (!$scope.image1) {
+      $scope.image1 = pokemon.image.href;
+    } else {
+      $scope.image2 = pokemon.image.href;
+    }
   }
 
 });
