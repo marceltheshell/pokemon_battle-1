cd76197ad7a736788587f5ca903c4499dfb30ea8
nathanallen <thenathanator@gmail.com>
2015-07-01 15:26:18 -0700

    naming things

--- base.js
+++ base.js
@@ -3,7 +3,7 @@ var app = angular.module('pokemonApp', []);
 app.controller('PokemonCtrl', function($scope) {
 
   $scope.pokemon = res.results.collection1; // see mock_data.js
-  $scope.setImage = function(src){
+  $scope.iChooseYou = function(src){
     $scope.image_src = src;
   }
 
