7c03b11f3e091c6b2a79529ecec49a6822f0a6d7
Marcel Degas <marcel.degas@gmail.com>
2015-10-17 16:36:37 -0700

    got the pokemons to show up on the page

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 var app = angular.module('pokemonApp', []);
 
-app.controller('PokemonCtrl', function($scope) {
+app.controller('PokemonCtrl', function($scope, PokemonLib) {
+	console.log("Pokemon index")
+	$scope.pokes = PokemonLib.query(); 
+	$scope.chooseFighter = function chooseFighter() {
+		if ($scope.battle[0]) {
+			$scope.battle.push(poke)
+		} 
+		else {
+			$scope.battle.push(poke)
+		}
+	}
+})
 
-});
+app.factory('PokemonLib', function(){
+
+  var PokemonLib = {};
+
+  PokemonLib.query = function(){
+    return pokemons.results.collection1;
+  }
+
+  // WineService.get = function(id){
+  //   var id = parseInt(id);
+  //   return ALL_WINES.find(function(wine){
+  //     return wine.id == id;
+  //   });
+  // }
+
+  return PokemonLib;
+
+})
